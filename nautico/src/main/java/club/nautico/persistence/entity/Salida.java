d7d06b650f4c8696c4303a2b537f565b215bfcf7
a926266 <a926266@DESKTOP-BDJPG0G>
2024-05-23 09:40:39 +0200

    no generar getters ni setters, que he hecho mal el anterior commit

--- nautico/src/main/java/club/nautico/persistence/entity/Salida.java
+++ nautico/src/main/java/club/nautico/persistence/entity/Salida.java
@@ -9,11 +9,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 
 public class Salida {
 	
