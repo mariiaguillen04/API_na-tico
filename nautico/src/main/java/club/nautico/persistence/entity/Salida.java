fa481da8d7a6bfd553cf73d63820ef1a8c343d9c
a926266 <a926266@DESKTOP-BDJPG0G>
2024-05-23 09:16:44 +0200

    tabla salida

--- nautico/src/main/java/club/nautico/persistence/entity/Salida.java
+++ nautico/src/main/java/club/nautico/persistence/entity/Salida.java
@@ -1,5 +1,23 @@
 package club.nautico.persistence.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+
 public class Salida {
+	
+	//Atributos	
+	
+	@Id
+	@GeneratedValue
+	private int idSalida;
+	private int idBarco;
+	private int idUser;
 
 }
