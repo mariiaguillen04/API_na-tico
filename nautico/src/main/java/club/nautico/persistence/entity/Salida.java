0f8987bfd762d5c952805052c8e84d480dd75e93
a926266 <a926266@DESKTOP-BDJPG0G>
2024-05-23 11:58:27 +0200

    one to many

--- nautico/src/main/java/club/nautico/persistence/entity/Salida.java
+++ nautico/src/main/java/club/nautico/persistence/entity/Salida.java
@@ -5,9 +5,12 @@ package club.nautico.persistence.entity;
 
 import javax.print.attribute.standard.DateTimeAtCompleted;
 
+import org.hibernate.annotations.CascadeType;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,7 +27,15 @@ public class Salida {
 	@Id
 	@GeneratedValue
 	private int id;
+	
+	// cascade = jakarta.persistence.CascadeType.ALL --> para que cualquier modificación relaionada se ejecute en todo lo relacionado
+	// orphanRemoval = true --> cuando elimine algo, se elimine todo lo relacionado
+	// Relación 1:n con tabla barco
+	@OneToMany(mappedBy = "salida", cascade = jakarta.persistence.CascadeType.ALL, orphanRemoval = true)
 	private int idBarco;
+	
+	// Relación 1:n con tabla usuario
+	@OneToMany(mappedBy = "salida", cascade = jakarta.persistence.CascadeType.ALL, orphanRemoval = true)
 	private int idUser;
 	private String destino;
 	private DateTimeAtCompleted fechaEntrada;
