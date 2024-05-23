1639e0907d61f18265c87d0c91b40162af9512dd
RubenPR2024 <rubprijur@gmail.com>
2024-05-23 11:32:32 +0200

    Commit - Creación clase Usuario

--- nautico/src/main/java/club/nautico/persistence/entity/Usuarios.java
+++ nautico/src/main/java/club/nautico/persistence/entity/Usuarios.java
@@ -1,5 +1,35 @@
 package club.nautico.persistence.entity;
 
+import javax.print.attribute.standard.DateTimeAtCompleted;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class Usuarios {
 
+	//Atributos Tabla Usuario
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String dni;
+	private String nombre;
+	private String apellido1;
+	private String apellido2;
+	private String telefono;
+	private String direccion;
+	private String email;
+	private String num_socio;
+	private String num_patron;
+	
+	
 }
