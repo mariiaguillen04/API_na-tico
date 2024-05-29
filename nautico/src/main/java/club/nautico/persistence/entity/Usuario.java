96b24313ba4cdeede35096aa0724fe923edb5e23
a926266 <a926266@DESKTOP-BDJPG0G>
2024-05-29 11:00:56 +0200

    num socio y patron y el model mapper

--- nautico/src/main/java/club/nautico/persistence/entity/Usuario.java
+++ nautico/src/main/java/club/nautico/persistence/entity/Usuario.java
@@ -21,6 +21,8 @@ public class Usuario {
 	private Long id;
 	private String dni;
 	private String nombre;
+	private String num_socio;
+	private String num_patron;
 	private String apellido1;
 	private String apellido2;
 	private String telefono;
