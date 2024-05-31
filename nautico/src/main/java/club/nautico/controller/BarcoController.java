6fb77cc3f59873672fd9ade1fd15bc834d9952e2
rleaoje99 <127490893+rleaoje99@users.noreply.github.com>
2024-05-31 13:19:05 +0200

    Commit barco controller

--- nautico/src/main/java/club/nautico/controller/BarcoController.java
+++ nautico/src/main/java/club/nautico/controller/BarcoController.java
@@ -1,14 +1,67 @@
 package club.nautico.controller;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import club.nautico.exception.ResourceNotFoundException;
 import club.nautico.persistence.entity.Barco;
+import club.nautico.persistence.repository.BarcoRepository;
 
 @RestController
+@RequestMapping("/api/v1/Barcos")
 public class BarcoController {
+	
+	@Autowired
+	private BarcoRepository BarcoRepository;
+	
+	@GetMapping
+	//Listar todos los barcos
+	public List<Barco> getAllBarcos() {
+		return BarcoRepository.findAll();
+	}
+	
+	@PostMapping
+	//Crear barco
+	public Barco createBarco(@RequestBody Barco Barco) {
+		return BarcoRepository.save(Barco);
+	}
+	
+	@GetMapping("{id}")
+	//Buscar Barco por id 
+	public ResponseEntity<Barco> getBarcoById(@PathVariable int id){
+		Barco Barco = BarcoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("El barco con id: "+ id+" no existe."));
+	return ResponseEntity.ok(Barco);
+	}
+	
+	@PutMapping("{id}")
+	//Modificar barco 
+	public ResponseEntity<Barco> updateUsuario(int id, Barco BarcoDetails) {
+		Barco updateBarco = BarcoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("El barco con id: "+ id+" no existe."));
+		updateBarco.setNombre(BarcoDetails.getNombre()); //BarcoDetails es nuestro "DTO"
+		updateBarco.setCuota(BarcoDetails.getCuota());
+		//Se podría ampliar el update
+		BarcoRepository.save(updateBarco);
+		return ResponseEntity.ok(updateBarco);
+	}
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity<HttpStatus> deleteBarco(@PathVariable int id) {
+		Barco Barco = BarcoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("El barco con id: "+ id+" no existe."));
+	BarcoRepository.delete(Barco);
+	return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
 	
 	
 }
