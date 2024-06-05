caa0dcdff18d382ebd1d8e2439908986b5e780bd
a926266 <a926266@10.234.93.171>
2024-06-05 13:42:02 +0200

    salida servicio

--- nautico/src/main/java/club/nautico/models/SalidaModel.java
+++ nautico/src/main/java/club/nautico/models/SalidaModel.java
@@ -2,6 +2,7 @@ package club.nautico.models;
 
 import javax.print.attribute.standard.DateTimeAtCompleted;
 
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,6 +10,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+
+
 
 @Entity
 @Table (name = "salida")
--- nautico/src/main/java/club/nautico/service/SalidaService.java
+++ nautico/src/main/java/club/nautico/service/SalidaService.java
@@ -2,6 +2,7 @@ package club.nautico.service;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.BeanUtils;
@@ -10,54 +11,68 @@ import org.springframework.stereotype.Service;
 
 import club.nautico.persistence.entity.Salida;
 import club.nautico.persistence.repository.SalidaRepository;
+import club.nautico.persistence.repository.UsuarioRepository;
 import club.nautico.service.dto.SalidaDto;
 
 @Service
 public class SalidaService {
 
 	@Autowired
-	private SalidaRepository salidaRepositorio;
+	private SalidaRepository salidaRepository;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
 
 	private final ModelMapper MODELMAPPER = new ModelMapper();
 
 	public SalidaDto convertToDto(Salida salida) {
 		return MODELMAPPER.map(salida, SalidaDto.class);
 	}
+	
+	
 
 	public Salida convertToEntity(SalidaDto salidaDto) {
 		return MODELMAPPER.map(salidaDto, Salida.class);
 	}
-
+	
+	
+//Métodos básicos
+	
 	public SalidaDto createSalida(SalidaDto salidaDto) {
-		// Guardamos ell DTO en una variable entidad
+		// Guardamos el DTO en una variable entidad
 		Salida salida = convertToEntity(salidaDto);
 		// Modificamos la tabla y almacenamos la nueva tabla
-		Salida salidaC = salidaRepositorio.save(salida);
+		Salida salidaC = salidaRepository.save(salida);
 		// Devolvemos el DTO de la nueva salida
-		return salidaRepositorio.convertToDto(salidaC);
+		return salidaRepository.convertToDto(salidaC);
 	}
 
+	
+	
 	public SalidaDto updateSalida(SalidaDto salidaDto, int id) {
 
 		// Busca la salida por ID, si no la encuentra, se lanza una excepción
-		Salida salidaUpdate = this.salidaRepositorio.findById(id)
+		Salida salidaUpdate = this.salidaRepository.findById(id)
 				.orElseThrow(() -> new RuntimeException("Salida not found with id " + id));
 
 		// Copia las propiedades del DTO de salida al modelo de salida
 		BeanUtils.copyProperties(salidaDto, salidaUpdate);
 
 		// Guarda el modelo de salida actualizado en la base de datos
-		Salida updatedSalida = this.salidaRepositorio.save(salidaUpdate);
+		Salida updatedSalida = this.salidaRepository.save(salidaUpdate);
 
 		// Convierte la salida actualizada a DTO
 		return convertToDto(updatedSalida);
 	}
+	
+	
 
 	public boolean deleteSalidaById(SalidaDto salidaDto, int id) {
 		boolean salidaDelete = false;
 
-		if (this.salidaRepositorio.findById(salidaDto.getId()).isPresent()) {
-			salidaRepositorio.deleteById(id);
+		if (this.salidaRepository.findById(salidaDto.getId()).isPresent()) {
+			salidaRepository.deleteById(id);
 			salidaDelete = true;
 		} else {
 			// excepción
@@ -65,9 +80,25 @@ public class SalidaService {
 		return salidaDelete;
 
 	}
+	
+	
+	public Optional<Salida> getSalidaById(int id) {
+		
+				if(this.salidaRepository.findById(id) != null) {
+					return salidaRepository.findById(id);
+				} else {
+					//Excepción
+					return null;
+				}
+	}
+	
+	/*public Optional<Salida> getSalidaByPatron(){
+		
+	}
+	*/
 
 	public List<Salida> findAllSalida() {
-		return this.salidaRepositorio.findAll();
+		return this.salidaRepository.findAll();
 	}
 
 }
